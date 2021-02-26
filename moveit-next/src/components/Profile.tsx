e70d3a0549e82cccc961d19a609dd9207c5bf1ce
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2021-02-26 17:57:51 -0300

    Update

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import { profile } from "console";
+
+export function Profile(){
+    return(
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/67983309?s=460&u=6fc5cf5a318083b443a4e5f499173a0ffb752685&v=4" alt="Guilherme Maciel"/>
+            <div>
+                <strong>Guilherme Maciel</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
